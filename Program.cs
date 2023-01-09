65636644eeff30858317e181fa7e6dd34667c595
Tom <artzhubrikov@mail.ru>
2023-01-09 09:51:48 +0300

    Task completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿string[] ArrayOfString = {"asdfsdj", "dfsdaf"};
-Console.WriteLine($"{ArrayOfString}");
+﻿string[] ArrayOfString = {"asdfsdj", "dfsdaf", "/", "asd", ";adjgji"}, 
+         ResultArray = new string[5];
+int i, j;
+Console.WriteLine($"[{string.Join(", ", ArrayOfString)}]");
+
+for (i = 0, j = 0; i < ArrayOfString.Length; i++) {
+    if (ArrayOfString[i].Length <= 3) {
+        ResultArray[j] = ArrayOfString[i];
+        j++;
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", ResultArray)}]");
